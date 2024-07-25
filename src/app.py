1efdc8e2ff56f9376bbab2b0c2cf92f43b7c1046
Gustavo Fernandez <66291521+Gfotero@users.noreply.github.com>
2024-07-25 22:17:58 +0000

    listo streamlit

--- src/app.py
+++ src/app.py
@@ -6,20 +6,23 @@ from pickle import load
 model = load(open("../models/random_forest_regressor_42_sin_scal.sav", "rb"))
 
 # Definir la interfaz de usuario
-st.title("Predicción de Precio de Viviendas")
-st.write("Ingresa los valores de las características:")
+st.markdown("<h2 style='color: blue;'>Predicción de Precio de Viviendas - Boston</h2>", unsafe_allow_html=True)
+st.write("Introducir Valores (Variables Tomadas en el Censo Ciudad de Boston - CRIM, RM, DIS, LSTAT)")
 
-crim = st.number_input("CRIM", min_value=0.0, max_value=100.0, value=0.0)
-rm = st.number_input("RM", min_value=1.0, max_value=10.0, value=5.0)
-dis = st.number_input("DIS", min_value=0.0, max_value=10.0, value=1.0)
-lstat = st.number_input("LSTAT", min_value=0.0, max_value=50.0, value=10.0)
+# Aumentar la precisión de las entradas a 4 decimales
+crim = st.text_input("CRIM", value="0.0000")
+rm = st.text_input("RM", value="1.0000")
+dis = st.text_input("DIS", value="1.0000")
+lstat = st.text_input("LSTAT", value="1.0000")
 
-# Crear un DataFrame con los valores ingresados
-data = pd.DataFrame([[crim, rm, dis, lstat]], columns=['CRIM', 'RM', 'DIS', 'LSTAT'])
+# Botón para realizar la predicción
+if st.button("Predecir"):
+    # Crear un DataFrame con los valores ingresados
+    data = pd.DataFrame([[float(crim), float(rm), float(dis), float(lstat)]], columns=['CRIM', 'RM', 'DIS', 'LSTAT'])
 
-# Realizar la predicción
-prediction = model.predict(data)[0]
-pred_class = f"{prediction:.3f} M$"
+    # Realizar la predicción
+    prediction = model.predict(data)[0]
+    pred_class = f"{prediction:.3f} M$"
 
-# Mostrar el resultado
-st.write(f"La predicción del precio de la vivienda es: {pred_class}")
+    # Mostrar el resultado
+    st.write(f"La predicción del precio de la vivienda es: {pred_class}")
